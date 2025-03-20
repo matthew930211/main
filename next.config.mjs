f6031d7560e4670e4a9c3d459feb6adb0e6faa50
matthew930211 <jacob.crotherz@gmail.com>
2025-03-20 11:24:38 +0100

    update modules for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,17 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["res.cloudinary.com", "cdn.bhlist.co.in", "images.clerk.dev", "cdn.discordapp.com", "images.unsplash.com", "localhost", "img.clerk.com", "192.168.1.19", "192.168.1.23","192.168.1.25"],
+        domains: ["res.cloudinary.com", "cdn.bhlist.co.in", "images.clerk.dev", "cdn.discordapp.com", "images.unsplash.com", "localhost", "img.clerk.com", "192.168.1.19", "192.168.1.23", "192.168.1.25"],
     },
     experimental: {
-        dynamicIO: true,
-        allowMiddlewareResponseBody: true
+        // dynamicIO: true,
+        // allowMiddlewareResponseBody: true,
+        // forceSwcTransforms: true,
     },
 };
 
 if (process.env.NODE_ENV === 'development') {
     await setupDevPlatform();
-  }
+}
 
 export default withNextVideo(nextConfig);
